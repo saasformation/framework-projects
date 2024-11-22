969ed9a3e23448d1a22ed479740d871ebd861626
SaaSFormation <info@saasformation.com>
2024-11-22 16:25:07 +0000

    feat: add uuids

--- src/Infrastructure/SymfonyContainerProvider.php
+++ src/Infrastructure/SymfonyContainerProvider.php
@@ -3,11 +3,13 @@
 namespace SaaSFormation\Framework\Projects\Infrastructure;
 
 use Exception;
+use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
 use SaaSFormation\Framework\Contracts\Infrastructure\ContainerProviderInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class SymfonyContainerProvider implements ContainerProviderInterface
@@ -23,12 +25,18 @@ class SymfonyContainerProvider implements ContainerProviderInterface
     {
         $container = new ContainerBuilder();
 
+        $container->set('default_logger', $logger);
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
         $loader->load($this->servicesFilePath);
 
-        $container->set('default_logger', $logger);
+        $container->setDefinition(Logger::class, (new Definition(Logger::class))->setSynthetic(true));
+        $container->setAlias('default_logger', Logger::class);
+        $container->setAlias(LoggerInterface::class, 'default_logger');
 
         $container->compile();
+
+        $container->set(Logger::class, $logger);
 
         return $container;
     }
