1c46487dc70bf39cef82eb554d1d3e96f8fba7cf
SaaSFormation <info@saasformation.com>
2024-11-23 10:42:39 +0000

    fix: command bus middleware was preventing custom middleware to be executed; merged both concepts

--- src/Infrastructure/Bus/CommandBusSendEventsToEventStreamMiddleware.php
+++ src/Infrastructure/Bus/CommandBusSendEventsToEventStreamMiddleware.php
@@ -2,12 +2,20 @@
 
 namespace SaaSFormation\Framework\Projects\Infrastructure\Bus;
 
+use League\Tactician\Exception\CanNotInvokeHandlerException;
+use League\Tactician\Handler\CommandNameExtractor\CommandNameExtractor;
+use League\Tactician\Handler\Locator\HandlerLocator;
+use League\Tactician\Handler\MethodNameInflector\MethodNameInflector;
 use League\Tactician\Middleware;
 use SaaSFormation\Framework\Contracts\Application\Bus\EventBusInterface;
 
 class CommandBusSendEventsToEventStreamMiddleware implements Middleware
 {
-    public function __construct(private EventBusInterface $eventBus)
+    public function __construct(
+        private EventBusInterface $eventBus,
+        private CommandNameExtractor $commandNameExtractor,
+        private HandlerLocator $handlerLocator,
+        private MethodNameInflector $methodNameInflector)
     {
     }
 
@@ -19,6 +27,19 @@ class CommandBusSendEventsToEventStreamMiddleware implements Middleware
             $this->eventBus->listen($event);
         }
 
-        return $domainEventStream;
+        $commandName = $this->commandNameExtractor->extract($command);
+        $handler = $this->handlerLocator->getHandlerForCommand($commandName);
+        $methodName = $this->methodNameInflector->inflect($command, $handler);
+
+        // is_callable is used here instead of method_exists, as method_exists
+        // will fail when given a handler that relies on __call.
+        if (!is_callable([$handler, $methodName])) {
+            throw CanNotInvokeHandlerException::forCommand(
+                $command,
+                "Method '{$methodName}' does not exist on handler"
+            );
+        }
+
+        return $handler->{$methodName}($command);
     }
 }
