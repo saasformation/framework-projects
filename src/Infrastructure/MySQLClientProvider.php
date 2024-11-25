fcd98a9b07b574edc9df90af0dbcfaae17316160
SaaSFormation <info@saasformation.com>
2024-11-25 08:29:17 +0000

    feat: add mysql based write model repository

--- src/Infrastructure/MySQLClientProvider.php
+++ src/Infrastructure/MySQLClientProvider.php
@@ -13,12 +13,22 @@ class MySQLClientProvider
     public function provide(): \PDO
     {
         $mysqlUri = $this->envVarsManager->get('MYSQL_URI');
+        $mysqlUsername = $this->envVarsManager->get('MYSQL_USERNAME');
+        $mysqlPassword = $this->envVarsManager->get('MYSQL_PASSWORD');
 
         if(!is_string($mysqlUri)) {
             throw new \InvalidArgumentException('MYSQL_URI must be a string');
         }
 
-        $pdo = new \PDO($mysqlUri);
+        if(!is_string($mysqlUsername)) {
+            throw new \InvalidArgumentException('MYSQL_USERNAME must be a string');
+        }
+
+        if(!is_string($mysqlPassword)) {
+            throw new \InvalidArgumentException('MYSQL_PASSWORD must be a string');
+        }
+
+        $pdo = new \PDO($mysqlUri, $mysqlUsername, $mysqlPassword);
         $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
