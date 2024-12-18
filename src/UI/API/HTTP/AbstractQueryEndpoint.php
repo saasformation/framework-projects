1692c1372d250575dd70cfad95691d9fb61e2644
SaaSFormation <info@saasformation.com>
2024-12-18 08:02:32 +0000

    feat: add mongodb transactions

--- src/UI/API/HTTP/AbstractQueryEndpoint.php
+++ src/UI/API/HTTP/AbstractQueryEndpoint.php
@@ -2,13 +2,21 @@
 
 namespace SaaSFormation\Framework\Projects\UI\API\HTTP;
 
+use Assert\Assert;
+use Psr\Http\Message\ServerRequestInterface;
 use SaaSFormation\Framework\SharedKernel\Application\Messages\QueryInterface;
 use SaaSFormation\Framework\SharedKernel\Application\Messages\QueryResultInterface;
+use SaaSFormation\Framework\SharedKernel\Common\Identity\IdInterface;
 
 abstract readonly class AbstractQueryEndpoint extends AbstractEndpoint
 {
-    public function ask(QueryInterface $query): QueryResultInterface
+    public function ask(QueryInterface $query, ServerRequestInterface $request): QueryResultInterface
     {
+        $requestId = $request->getAttribute('request_id');
+        Assert::that($requestId)->isInstanceOf(IdInterface::class);
+
+        $query->setRequestId($requestId);
+
         return $this->queryBus->ask($query);
     }
 }
